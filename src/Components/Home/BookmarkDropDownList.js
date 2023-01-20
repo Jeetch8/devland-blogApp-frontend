38183a6032745682095b9dfff8eda1886fb349d4
Jeet Chawda <jeetkumar0898@gmail.com>
2023-01-20 21:33:11 +0530

    adding and removing blog to booking category

--- src/Components/Home/BookmarkDropDownList.js
+++ src/Components/Home/BookmarkDropDownList.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const BookmarkDropDownList = ({ el, blogId }) => {
+  const [boxChecked, setBoxChecked] = useState(false);
+  const sendbookmarkReqFunction = (res) => {
+    if (res.target.checked) {
+      axios
+        .post("/user/addBlogsToCategory", { catId: el.catId, blogId })
+        .then((res) => console.log(res.data));
+    } else if (!res.target.checked) {
+      axios
+        .post("/user/removeBlogFromBookmarkCategory", {
+          catId: el.catId,
+          blogId,
+        })
+        .then((res) => console.log(res.data));
+    }
+  };
   return (
     <>
       <li className="px-4 py-1 flex items-center">
@@ -8,6 +24,7 @@ const BookmarkDropDownList = ({ el, blogId }) => {
           type={"checkbox"}
           className="cursor-pointer mr-2"
           defaultChecked={el.isSaved ? true : false}
+          onChange={sendbookmarkReqFunction}
         />
         <h2>{el.catTitle}</h2>
       </li>
