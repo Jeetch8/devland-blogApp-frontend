7b06b65fbdef5a30c182251d952cf46824504187
Jeetch8 <jchawda332@gmail.com>
2023-04-19 20:17:14 +0530

    Adding backend

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { Grid } from "@mui/material";
 import BlogCard from "../components/BlogCard";
+import { useState } from "react";
 
 const Home = () => {
+  const [allBlogs, setAllBlogs] = useState([]);
+
   return (
     <div>
       <Grid>{/* <BlogCard  />     */}</Grid>
